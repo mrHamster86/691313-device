728f639d5b919b0836b47c47860962b6e483c5ae
Evgeniy <39721548+mrHamster86@users.noreply.github.com>
2018-06-27 19:31:26 +0300

    Все кроме js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//service-slider
+ /*$(document).ready(function() {
+
+//modal
+    $('.min-map').click(function(){
+        $('.modal').show();
+        $('.map').show();
+    });
+    $('.contacts__btn').click(function(){
+        $('.modal').show();
+        $('.write-us').show();
+    });
+    $('.modal__close').click(function(){
+        $('.modal').hide();
+        $('.modal').children().hide();
+    });
+});
